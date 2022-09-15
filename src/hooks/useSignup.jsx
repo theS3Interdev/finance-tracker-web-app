c3485a03f959cd64715c99a454ef0aa4c8ef3150
Michael Owuor <michael.owuor@gmail.com>
2022-09-15 23:38:55 +0300

    feat: create and use signup hook (#13)
    * feat: create and use signup hook
    
    * fix: miscellaneous fixes
    
    * fix: miscellaneous fixes

---
+++ src/hooks/useSignup.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { projectAuth } from '../firebase/config';
+
+export const useSignup = () => {
+	const [error, setError] = useState(null);
+	const [isPending, setIsPending] = useState(false);
+
+	const signup = async (email, password, displayName) => {
+		setError(null);
+		setIsPending(true);
+
+		try {
+			/** signup procedure */
+			const res = await projectAuth.createUserWithEmailAndPassword(email, password);
+
+			if (!res) {
+				throw new Error('Could not complete the new user sign up');
+			}
+
+			/** add the display name to user profile */
+			await res.user.updateProfile({ displayName });
+
+			setIsPending(false);
+			setError(null);
+		} catch (err) {
+			console.log(err.message);
+			setError(err.message);
+			setIsPending(false);
+		}
+	};
+
+	return { error, isPending, signup };
+};
--- src/pages/signup/sign-up.jsx
+++ src/pages/signup/sign-up.jsx
@@ -1,39 +1,60 @@
 import { useState } from 'react';
+import { useSignup } from '../../hooks/useSignup';
 
 const Signup = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [displayName, setDisplayName] = useState('');
+	const { error, isPending, signup } = useSignup();
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(email, password, displayName);
+		signup(email, password, displayName);
 	};
 
 	return (
 		<form onSubmit={handleSubmit} className="signup-form">
 			<h2>Sign Up</h2>
+
 			<label>
 				<span>Email:</span>
-				<input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+				<input
+					type="email"
+					onChange={(e) => setEmail(e.target.value)}
+					value={email}
+					required
+				/>
 			</label>
+
 			<label>
 				<span>Password:</span>
 				<input
 					type="password"
 					onChange={(e) => setPassword(e.target.value)}
 					value={password}
+					required
 				/>
 			</label>
+
 			<label>
 				<span>Display Name:</span>
 				<input
 					type="text"
 					onChange={(e) => setDisplayName(e.target.value)}
 					value={displayName}
+					required
 				/>
 			</label>
-			<button className="btn">Sign Up</button>
+
+			{!isPending && <button className="btn">Sign Up</button>}
+
+			{isPending && (
+				<button className="btn" disabled>
+					Loading...
+				</button>
+			)}
+
+			{error && <p>{error}</p>}
 		</form>
 	);
 };
