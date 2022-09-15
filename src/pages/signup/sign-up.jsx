cefff326b00df4d4c2c4d711b6a18e086c81416a
Michael Owuor <michael.owuor@gmail.com>
2022-09-15 21:03:54 +0300

    feat: create sign up form (#9)

--- src/pages/signup/sign-up.jsx
+++ src/pages/signup/sign-up.jsx
@@ -1,4 +1,41 @@
+import { useState } from 'react';
+
 const Signup = () => {
-	return <div>Sign Up</div>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [displayName, setDisplayName] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password, displayName);
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="signup-form">
+			<h2>Sign Up</h2>
+			<label>
+				<span>Email:</span>
+				<input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+			</label>
+			<label>
+				<span>Password:</span>
+				<input
+					type="password"
+					onChange={(e) => setPassword(e.target.value)}
+					value={password}
+				/>
+			</label>
+			<label>
+				<span>Display Name:</span>
+				<input
+					type="text"
+					onChange={(e) => setDisplayName(e.target.value)}
+					value={displayName}
+				/>
+			</label>
+			<button className="btn">Sign Up</button>
+		</form>
+	);
 };
+
 export default Signup;
