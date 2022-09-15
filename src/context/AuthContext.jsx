4893cfce2d55a8c15547b1fed13bfe523fb2498e
Michael Owuor <michael.owuor@gmail.com>
2022-09-16 00:50:44 +0300

    feat: dispatch a signin action

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,6 +4,8 @@ export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
 	switch (action.type) {
+		case 'SIGNIN':
+			return { ...state, user: action.payload };
 		default:
 			return state;
 	}
