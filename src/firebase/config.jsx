dd6a37c85e1ea0b3a35d8c9cfb2b3620d9e83aa1
Michael Owuor <michael.owuor@gmail.com>
2022-09-15 21:31:25 +0300

    chore: firebase cloud firestore setup

---
+++ src/firebase/config.jsx
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+/** firebase configuration object containing keys and identifiers the app */
+const firebaseConfig = {
+	apiKey: 'AIzaSyAPBfUPt7JhYsmEzlCQreCss3YLPFmwl-4',
+	authDomain: 'finance-tracker-by-s3.firebaseapp.com',
+	projectId: 'finance-tracker-by-s3',
+	storageBucket: 'finance-tracker-by-s3.appspot.com',
+	messagingSenderId: '910930852697',
+	appId: '1:910930852697:web:2b9c43cf089b9912812a48',
+	measurementId: 'G-TLPC11TMWE',
+};
+
+/** initialize firebase */
+firebase.initializeApp(firebaseConfig);
+
+/** initialize firebase services */
+const projectDB = firebase.firestore();
+
+/** export services */
+export { projectDB };
