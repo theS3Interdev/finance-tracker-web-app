ac61cf1d5154cc81db74eadb88e7cb49b8f5a118
Michael Owuor <michael.owuor@gmail.com>
2022-09-16 04:12:13 +0300

    feat: conditional showing of user content (#17)
    * feat: conditional showing of user content
    
    * feat: waiting until auth is ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
 import { useSignout } from '../hooks/useSignout';
 
 const Navbar = () => {
 	const { signout } = useSignout();
+	const { user } = useAuthContext();
 
 	return (
 		<nav className="navbar">
@@ -10,17 +12,28 @@ const Navbar = () => {
 				<li className="title">
 					<Link to="/">Finance Tracker</Link>
 				</li>
-				<li>
-					<Link to="/signin">Sign In</Link>
-				</li>
-				<li>
-					<Link to="/signup">Sign Up</Link>
-				</li>
-				<li>
-					<button className="btn" onClick={signout}>
-						Sign Out
-					</button>
-				</li>
+
+				{!user && (
+					<>
+						<li>
+							<Link to="/signin">Sign In</Link>
+						</li>
+						<li>
+							<Link to="/signup">Sign Up</Link>
+						</li>
+					</>
+				)}
+
+				{user && (
+					<>
+						<li>Hello, {user.displayName}</li>
+						<li>
+							<button className="btn" onClick={signout}>
+								Sign Out
+							</button>
+						</li>
+					</>
+				)}
 			</ul>
 		</nav>
 	);
