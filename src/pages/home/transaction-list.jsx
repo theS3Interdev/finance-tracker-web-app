7b9471229c0c3bbbbd47f446012f88b9077ee812
Michael Owuor <michael.owuor@gmail.com>
2022-09-19 11:49:21 +0300

    feat: listing transactions

---
+++ src/pages/home/transaction-list.jsx
@@ -0,0 +1,14 @@
+const TransactionList = ({ transactions }) => {
+	return (
+		<ul className="transactions">
+			{transactions.map((transaction) => (
+				<li key={transaction.id}>
+					<p className="name">{transaction.name}</p>
+					<p className="amount">${transaction.amount}</p>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default TransactionList;
