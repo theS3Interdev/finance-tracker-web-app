cbcd0bc7e2f0849fe4784833c325847f9846284f
Michael Owuor <michael.owuor@gmail.com>
2022-09-19 13:40:09 +0300

    feat: deleting transactions (#39)

--- src/pages/home/transaction-list.jsx
+++ src/pages/home/transaction-list.jsx
@@ -1,10 +1,15 @@
+import { useFirestore } from '../../hooks/useFirestore';
+
 const TransactionList = ({ transactions }) => {
+	const { deleteDocument } = useFirestore('transactions');
+
 	return (
 		<ul className="transactions">
 			{transactions.map((transaction) => (
 				<li key={transaction.id}>
 					<p className="name">{transaction.name}</p>
 					<p className="amount">KES{transaction.amount}</p>
+					<button onClick={() => deleteDocument(transaction.id)}>x</button>
 				</li>
 			))}
 		</ul>
