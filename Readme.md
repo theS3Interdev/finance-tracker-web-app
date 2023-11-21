bf99e56ea3d0a4a11d5eba22bb086aac34d0de4a
Michael Owuor <michael.owuor@gmail.com>
2023-11-21 08:49:51 +0300

    chore: update dependencies

--- Readme.md
+++ Readme.md
@@ -10,9 +10,9 @@ React.JS project created to learn about
 1. useEffect hook
 1. React.JS Router
 1. React.JS Context and Reducers
-1. Firebase Cloud Firestore and Hosting
-
-The project also puts into practice proper management of Git by organizing code into staging and production ready branches so that there is always a version of the code ready to go.
+1. Firebase Cloud Firestore
+1. GitHub Actions
+1. GitHub Pages
 
 Finally, the repetitive process of building and deploying the project to production is automated by using GitHub Actions for onward deployment to Firebase Hosting.
 
@@ -44,5 +44,5 @@ yarn dev
 1. Tailwind CSS
 1. Vite.JS
 1. Firebase Cloud Firestore
-1. Firebase Hosting
+1. GitHub Pages
 1. GitHub Actions
--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 
 /** firebase configuration object containing keys and identifiers the app */
 const firebaseConfig = {
-	apiKey: 'AIzaSyAPBfUPt7JhYsmEzlCQreCss3YLPFmwl-4',
-	authDomain: 'finance-tracker-by-s3.firebaseapp.com',
-	projectId: 'finance-tracker-by-s3',
-	storageBucket: 'finance-tracker-by-s3.appspot.com',
-	messagingSenderId: '910930852697',
-	appId: '1:910930852697:web:2b9c43cf089b9912812a48',
-	measurementId: 'G-TLPC11TMWE',
+  apiKey: "AIzaSyCMayhCmpGb7sHYkVkSntFcrX2ux9lCzPI",
+  authDomain: "the-finance-tracker-project.firebaseapp.com",
+  projectId: "the-finance-tracker-project",
+  storageBucket: "the-finance-tracker-project.appspot.com",
+  messagingSenderId: "545582212749",
+  appId: "1:545582212749:web:f0ce90019dd1436906a4c4",
+  measurementId: "G-ECC6483P3Q"
 };
 
 /** initialize firebase */
