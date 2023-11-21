33853a4109b6c94f2ae91b3440a11c2a2db1bd87
Michael Owuor <michael.owuor@gmail.com>
2023-11-21 09:13:09 +0300

    fix: create transaction index

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		fontFamily: {
-			ubuntu: 'Ubuntu, sans-serif',
-		},
-		extend: {
-			colors: {
-				darkCharcoal: '#333333',
-				charcoal: '#555555',
-				lightCharcoal: '#777777',
-				darkGreen: '#1F9751',
-				lightGreen: '#EFFAF0',
-				whiteSmoke: '#F2F2F2',
-				lightGray: '#dddddd',
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/aspect-ratio'),
-		require('@tailwindcss/forms'),
-		require('@tailwindcss/line-clamp'),
-		require('@tailwindcss/typography'),
-	],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      ubuntu: "Ubuntu, sans-serif",
+    },
+    extend: {
+      colors: {
+        darkCharcoal: "#333333",
+        charcoal: "#555555",
+        lightCharcoal: "#777777",
+        darkGreen: "#1F9751",
+        lightGreen: "#EFFAF0",
+        whiteSmoke: "#F2F2F2",
+        lightGray: "#dddddd",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
